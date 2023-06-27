15487a83cc024b4fa9e65f8bfe4e88f43052cf91
Uchida <remem20130823@gmail.com>
2023-06-27 14:39:55 +0900

    Quiz系

--- src/main/java/com/design_shinbi/circle/model/Quiz.java
+++ src/main/java/com/design_shinbi/circle/model/Quiz.java
@@ -9,22 +9,38 @@ public class Quiz {
 	private List<Question> questions;
 	private int correctCount;
 	private QuizDAO dao;
+	private List<Integer> history;
+	
+	/*
+	 * state
+	 * 0:クイズスタート前
+	 * 1:クイズ出題
+	 * 2:クイズ答え合わせ
+	 * 4:クイズ終了
+	 */
 	private int state;
 		
-	public Quiz(QuizDAO dao) {
+	public Quiz(QuizDAO dao) throws SQLException {
 		this.dao = dao;
+		this.init();
 	}
 	
 	public void init() throws SQLException {
 		this.questions = dao.choiceQuestions();
 		//データベースのrand()でシャッフルされているのでコメントアウト
 		//Collections.shuffle(questions);
+		correctCount = 0;
+		state = 0;
 	}
 	
-	public String ask() {
+	public Question ask() {
+		
 		return null;
 	}
 	
+	public int getState() {
+		return state;
+	}
 	
 	public int getCorrectCount() {
 		return correctCount;
--- src/main/java/com/design_shinbi/circle/servlet/QuizServlet.java
+++ src/main/java/com/design_shinbi/circle/servlet/QuizServlet.java
@@ -9,13 +9,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.design_shinbi.circle.model.Quiz;
+
 @WebServlet("/play")
 public class QuizServlet extends HttpServlet{
+	private Quiz quiz;
 
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String jsp = null;
 		HttpSession session = req.getSession();
-
+		
 		
 	}
 	
