6d67705a2bfc8452f6e3f00a6ca4ff261f054d48
Ryo Yokoo <itiryo090702@icloud.com>
2023-07-11 11:36:26 +0900

    image/image_file_nameフィールドの追加とsetIconの追加

--- schema.sql
+++ schema.sql
@@ -1,5 +1,4 @@
 SET SESSION FOREIGN_KEY_CHECKS=0;
-SET sql_mode = ALLOW_INVALID_DATES;
 
 /* Drop Tables */
 
@@ -8,18 +7,21 @@ DROP TABLE IF EXISTS ranking;
 DROP TABLE IF EXISTS result;
 DROP TABLE IF EXISTS users;
 
+
+
+
 /* Create Tables */
 
 CREATE TABLE questions
 (
 	id int NOT NULL AUTO_INCREMENT,
 	sentence text NOT NULL,
-	genre varchar(64) NOT NULL,
 	correct text NOT NULL,
 	choice2 text NOT NULL,
 	choice3 text NOT NULL,
 	choice4 text NOT NULL,
 	imagec mediumblob,
+	genre varchar(64) NOT NULL,
 	image2 mediumblob,
 	image3 mediumblob,
 	image4 mediumblob,
@@ -34,7 +36,7 @@ CREATE TABLE ranking
 	correctValue int NOT NULL,
 	questionValue int NOT NULL,
 	time bigint NOT NULL,
-	created_at timestamp(3) NOT NULL,
+	created_at timestamp NOT NULL,
 	PRIMARY KEY (id)
 );
 
@@ -44,7 +46,7 @@ CREATE TABLE result
 	id int NOT NULL AUTO_INCREMENT,
 	user_id int NOT NULL,
 	score int NOT NULL,
-	created_at timestamp(3) NOT NULL,
+	created_at timestamp NOT NULL,
 	PRIMARY KEY (id)
 );
 
@@ -58,6 +60,10 @@ CREATE TABLE users
 	is_admin boolean NOT NULL,
 	created_at timestamp NOT NULL,
 	updated_at timestamp NOT NULL,
+	-- 画像データ
+	icon mediumblob COMMENT '画像データ',
+	-- 画像データのファイル名
+	icon_file_name varchar(64) COMMENT '画像データのファイル名',
 	PRIMARY KEY (id),
 	UNIQUE (email),
 	UNIQUE (name)
