ef65c5d86db319a4b9472fd2ff1e4930a40600a2
Uchida <remem20130823@gmail.com>
2023-07-12 14:24:16 +0900

    継承先変更

--- src/main/java/com/design_shinbi/circle/servlet/UploadIconServlet.java
+++ src/main/java/com/design_shinbi/circle/servlet/UploadIconServlet.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -16,7 +15,7 @@ import com.design_shinbi.circle.model.dao.UserDAO;
 import com.design_shinbi.circle.model.entity.User;
 
 @WebServlet("/upload")
-public class UploadIconServlet extends HttpServlet {
+public class UploadIconServlet extends BaseServlet {
 
 	private static final long serialVersionUID = 1L;
 
--- src/main/java/com/design_shinbi/circle/servlet/UserServlet.java
+++ src/main/java/com/design_shinbi/circle/servlet/UserServlet.java
@@ -5,18 +5,15 @@ import java.io.IOException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.servlet.http.Part;
 
 import com.design_shinbi.circle.model.Const;
-import com.design_shinbi.circle.model.dao.UserDAO;
 import com.design_shinbi.circle.model.entity.User;
 
 @WebServlet("/user")
-public class UserServlet extends HttpServlet {
+public class UserServlet extends BaseServlet {
 
 	private static final long serialVersionUID = 1L;
 
@@ -40,18 +37,18 @@ public class UserServlet extends HttpServlet {
 		dispatcher.forward(request, reaponse);
 	}
 
-	private void setIcon(HttpServletRequest req, UserDAO dao, User entity) throws Exception {
-		Part part = req.getPart("icon_file");
-		String fileName = part.getSubmittedFileName();
-		boolean deleteFlag = Boolean.parseBoolean(req.getParameter("delete_icon_flag"));
-
-		if (fileName == null || fileName.isEmpty()) {
-			if (deleteFlag) {
-				dao.removeIcon(entity.getId());
-			}
-		} else {
-			dao.setIcon(entity.getId(), fileName, part.getInputStream());
-		}
-	}
+//	private void setIcon(HttpServletRequest req, UserDAO dao, User entity) throws Exception {
+//		Part part = req.getPart("icon_file");
+//		String fileName = part.getSubmittedFileName();
+//		boolean deleteFlag = Boolean.parseBoolean(req.getParameter("delete_icon_flag"));
+//
+//		if (fileName == null || fileName.isEmpty()) {
+//			if (deleteFlag) {
+//				dao.removeIcon(entity.getId());
+//			}
+//		} else {
+//			dao.setIcon(entity.getId(), fileName, part.getInputStream());
+//		}
+//	}
 
 }
